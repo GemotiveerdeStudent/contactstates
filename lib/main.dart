3d938fcf08180da985859a6551ecfca8ef8bf125
Bradley Lasschuit <129509756+GemotiveerdeStudent@users.noreply.github.com>
2025-01-21 22:17:15 +0100

    added valuelistener

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -17,35 +18,39 @@ void main() {
 }
 
 class Contact {
+  final String id;
   final String name;
-
-  const Contact({
+  Contact({
     required this.name,
-  });
+  }) : id = const Uuid().v4();
 }
 
-class ContactBook {
+class ContactBook extends ValueNotifier<List<Contact>> {
   // Singleton pattern for contactbook
-  ContactBook._sharedInstance();
+  ContactBook._sharedInstance() : super([]);
   static final ContactBook _shared = ContactBook._sharedInstance();
   factory ContactBook() => _shared;
 
   final List<Contact> _contacts = []; // Initialize empty list of contacts
 
-  int get length => _contacts.length; // Getter for length of contacts
+  int get length => value.length; // Getter for length of contacts
 
   void add({required Contact contact}) {
-    // Add contact to list
-    _contacts.add(contact);
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
   }
 
   void remove({required Contact contact}) {
-    // Remove contact from list
-    _contacts.remove(contact);
+    final contacts = value; // Remove contact from list
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
   }
 
   Contact? contact({required int atIndex}) => // Get contact at index
-      _contacts.length > atIndex ? _contacts[atIndex] : null;
+      value.length > atIndex ? value[atIndex] : null;
 }
 
 class HomePage extends StatelessWidget {
