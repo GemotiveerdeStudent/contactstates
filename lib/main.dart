4a69ec085fc974387d32b118c6979e149faf28c2
Bradley Lasschuit <129509756+GemotiveerdeStudent@users.noreply.github.com>
2025-01-21 17:35:51 +0100

    Added standard homepage phonebook list & Add button

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,43 @@ void main() {
       ),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
+      routes: {
+        '/newContact': (context) => const NewContactView(),
+      },
     ),
   );
+}
+
+class Contact {
+  final String name;
+
+  const Contact({
+    required this.name,
+  });
+}
+
+class ContactBook {
+  // Singleton pattern for contactbook
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = []; // Initialize empty list of contacts
+
+  int get length => _contacts.length; // Getter for length of contacts
+
+  void add({required Contact contact}) {
+    // Add contact to list
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    // Remove contact from list
+    _contacts.remove(contact);
+  }
+
+  Contact? contact({required int atIndex}) => // Get contact at index
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
 }
 
 class HomePage extends StatelessWidget {
@@ -18,9 +53,80 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final contactBook = ContactBook();
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: const Text('Home Page'),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/newContact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// + Contact button
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('New Contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter contact name here....',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Add Contact',
+            ),
+          ),
+        ],
       ),
     );
   }
